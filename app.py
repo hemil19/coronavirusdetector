40d35e4153271841c06b4849fe7d985f89dac67a
Hemil Shah <hemilshah68@gmail.com>
2020-10-07 02:43:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import sys
+import os
+import glob
+import re
+import numpy as np
+import tensorflow
+
+# Keras
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPool2D,Dense,Dropout
+
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH ='covid19_xray_resnet_50.h5'
+
+# Load your trained model
+
+model = load_model(MODEL_PATH)
+
+
+
+def model_predict(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    
+    x=x/255
+    x = np.expand_dims(x, axis=0)
+    print(x.shape)
+    preds = model.predict(x)
+    
+    if preds>=0.5:
+        preds="Covid-Positive"
+    elif preds==1:
+        preds="Covid-Negative"
+
+    
+    
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        
+        f = request.files['file']
+
+        
+        basepath = os.path.dirname(__file__)
+        if not os.path.exists('uploads'):
+            os.mkdir('uploads')
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        #Prediction
+        preds = model_predict(file_path)
+        result=preds
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
